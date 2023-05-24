8107246501e57ec82b647f5befb948c713a7e07f
Dolly Bhardwaj <100553036+do-lly@users.noreply.github.com>
2023-05-24 23:16:55 +0530

    Add files via upload

---
+++ part1/src/index.js
@@ -0,0 +1,18 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+ReactDOM.render(
+  <>
+  <h1>Top Netflix movies</h1>
+  <p>Dolly's favorite Movies</p>
+  <ol>
+    <li>THE SHUTTER ISLAND</li>
+    <li>FRACTURED</li>
+    <li>RUN</li>
+    <li>THE MIST</li>
+    <li>THE SILENCE</li>
+  </ol>
+
+  </>
+  ,document.getElementById("root")
+  
+)
